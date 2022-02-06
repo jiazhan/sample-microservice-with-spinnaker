d59f090d312f6efe84c7bcd7dd0175c22aade05f
kevin zhang <jia.zhang@everbridge.com>
2022-02-06 19:56:52 +0800

    update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // HandlerFunc1 handles basic calls
 func HandlerFunc1(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "\nKevin updated Release 1.012")
+	fmt.Fprintf(w, "\nKevin updated Release 1.015")
 }
 
 func main() {
